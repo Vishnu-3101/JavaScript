a425f885527f532e1f3d0af9759347bd6904807f
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2023-11-07 17:24:43 +0530

    Updated files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This repo consists of code to create a login page with HTML, CSS and JavaScript
+This repo started with creating a login page with HTML, CSS and JavaScript.<br/>
+The current code consists has a backend server which connects to the database for authentication.<br/>
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// We dont need this file after I made changes to the code to connect to backend server
+// But this one would be useful if we want to test the front end part without and backend
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
